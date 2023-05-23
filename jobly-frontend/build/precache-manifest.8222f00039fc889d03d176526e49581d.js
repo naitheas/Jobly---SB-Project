34a8d64dc6c155cb5e2fdb492e95bac13dd288d6
Amber Ellett <80650755+naitheas@users.noreply.github.com>
2023-05-23 12:44:24 -0400

    Add files via upload

---
+++ jobly-frontend/build/precache-manifest.8222f00039fc889d03d176526e49581d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ca0add4724fe81e3a0df83a92737da2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0c5559dbeff3a992cce",
+    "url": "/static/css/2.5aa997f2.chunk.css"
+  },
+  {
+    "revision": "64be10b0a39be0d69578",
+    "url": "/static/css/main.f40adb53.chunk.css"
+  },
+  {
+    "revision": "f0c5559dbeff3a992cce",
+    "url": "/static/js/2.a8083301.chunk.js"
+  },
+  {
+    "revision": "395cc4b3602716a6dc8075912d30f948",
+    "url": "/static/js/2.a8083301.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64be10b0a39be0d69578",
+    "url": "/static/js/main.323e7736.chunk.js"
+  },
+  {
+    "revision": "96d14116823ca0bb5594",
+    "url": "/static/js/runtime-main.159b7ccd.js"
+  }
+]);
