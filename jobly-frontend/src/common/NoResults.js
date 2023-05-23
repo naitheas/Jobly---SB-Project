792e72c9baf63ac71e3ff76247e45ab564f71cfc
Amber Ellett <80650755+naitheas@users.noreply.github.com>
2023-05-23 12:45:12 -0400

    Add files via upload

---
+++ jobly-frontend/src/common/NoResults.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./NoResults.css";
+
+const NoResults = () => {
+  return (
+    <div className="NoResults">
+      
+     Sorry, no results found.
+      
+    </div>
+  )
+}
+
+export default NoResults;
